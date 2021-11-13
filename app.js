3623e787cdcb89c1c399cc4f77dd61d33373102b
Karytonn <karytonndev@gmail.com>
2021-11-13 14:13:50 +0000

    Update project

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 document.getElementById("send").addEventListener("click", function(event){
     event.preventDefault()
 
+    console.log("Sending message...")
+
     //get input data
     const name = document.querySelector("#fname")
     const email = document.querySelector("#fmail")
@@ -9,7 +11,7 @@ document.getElementById("send").addEventListener("click", function(event){
     const message = document.querySelector("#fmessage")
 
     //format text to send with WhatsApp
-    let textToUrl = `(${name.value} - ${email.value} - ${city.value}) --> ${message.value}`
+    let textToUrl = `[${name.value} - ${email.value} - ${city.value}] --> ${message.value}`
     textToUrl.replace(/\s+/g, "%20")
     
     //Set your WhatsApp number
